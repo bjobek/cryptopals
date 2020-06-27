c0185537bf6e388c64f772dbf55e615c733130b8
bjobek <bjornivar95@gmail.com>
2020-06-27 06:52:50 +0200

    added new folder structure for c/cxx

---
+++ c/set1/set1.c
@@ -0,0 +1,23 @@
+#include "set1.h"
+
+
+
+int hexToByte(byte * buf, size_t len)
+{
+
+}
+
+
+
+int hexToByteMap()
+{
+
+}
+
+
+
+int byteToHex()
+{
+
+}
+
---
+++ c/set1/set1.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+typedef  unsigned char byte;
+
+int hexToByte(byte *buf, size_t len);
+
+
+
+
+
+int byteToHex();
+
+int hexToByteMap();
+
+// hex string to bits/bytes
+// xor operation
+//produce hex string again from bits
+
+// make a hex to byte converter
+
